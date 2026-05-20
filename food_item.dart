e6964e370a8c5b8a370d85d5acdb3231356ec5e0
aditisharma65721-svg <aditisharma65721@gmail.com>
2026-05-20 17:04:18 +0530

    food_item.dart

---
+++ food_item.dart
@@ -0,0 +1,14 @@
+
+class FoodItem {
+  final String name;
+  final String imageUrl;
+  final double price;
+  final String description;
+
+  FoodItem({
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+    required this.description,
+  });
+}
